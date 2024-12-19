986feb3929888a729fd8ec47320ab144856a3a57
Jakub Łobos <jakubloboskontakt@gmail.com>
2024-12-19 17:05:11 +0100

    fix eslint

--- src/modules/users/components/Profile/index.tsx
+++ src/modules/users/components/Profile/index.tsx
@@ -1,64 +1,63 @@
 import { Box, Typography } from "@mui/material";
 import { Roles } from "../../constants";
 import ChangeAvatar from "../ChangeAvatar";
-import { useState } from "react";
 
 interface Props {
-  username: string;
-  role: Roles;
-  avatar_url?: string;
-  isOwner: boolean;
-  onSubmit: (values: { avatar: string }) => void;
+    username: string;
+    role: Roles;
+    avatar_url?: string;
+    isOwner: boolean;
+    onSubmit: (values: { avatar: string }) => void;
 }
 
 const UserProfileHeading = ({
-  role,
-  username,
-  avatar_url,
-  isOwner,
-  onSubmit,
+    role,
+    username,
+    avatar_url,
+    isOwner,
+    onSubmit,
 }: Props) => {
-  return (
-    <Box>
-      <Box
-        sx={{
-          display: "flex",
-          flexDirection: "row",
-          alignItems: "flex-end",
-          gap: "20px",
-        }}
-      >
-        <ChangeAvatar
-          disabled={!isOwner}
-          avatar={avatar_url}
-          username={username}
-          onSubmit={onSubmit}
-        />
+    return (
         <Box>
-          <Typography
-            color="text.secondary"
-            sx={{
-              fontSize: "24px",
-              fontWeight: "bold",
-              textTransform: "uppercase",
-            }}
-          >
-            {username}
-          </Typography>
-          <Typography
-            color="text.secondary"
-            sx={{
-              fontSize: "20px",
-              fontWeight: "bold",
-              textTransform: "uppercase",
-            }}
-          >
-            {role}
-          </Typography>
+            <Box
+                sx={{
+                    display: "flex",
+                    flexDirection: "row",
+                    alignItems: "flex-end",
+                    gap: "20px",
+                }}
+            >
+                <ChangeAvatar
+                    disabled={!isOwner}
+                    avatar={avatar_url}
+                    username={username}
+                    onSubmit={onSubmit}
+                />
+                <Box>
+                    <Typography
+                        color="text.secondary"
+                        sx={{
+                            fontSize: "24px",
+                            fontWeight: "bold",
+                            textTransform: "uppercase",
+                        }}
+                    >
+                        {username}
+                    </Typography>
+                    <Typography
+                        color="text.secondary"
+                        sx={{
+                            fontSize: "20px",
+                            fontWeight: "bold",
+                            textTransform: "uppercase",
+                        }}
+                    >
+                        {role}
+                    </Typography>
+                </Box>
+            </Box>
         </Box>
-      </Box>
-    </Box>
-  );
+    );
 };
 
 export default UserProfileHeading;
