feddc8e4465892bdb6a38116cf2dd2a2fbcd0874
Vardy41 <kokoko1325@gmail.com>
2025-01-25 16:14:06 +0100

    Poprawka podstrony 404, centrowanie napisu i zrobienie RWD dla podstrony

--- src/screens/NotFoundScreen.tsx
+++ src/screens/NotFoundScreen.tsx
@@ -1,19 +1,48 @@
-import { Typography } from "@mui/material";
-import Container from "../modules/shared/components/Container";
-import { useTranslation } from "react-i18next";
-import Layout from "../modules/shared/components/Layout";
+import { Typography } from '@mui/material';
+import Container from '../modules/shared/components/Container';
+import { useTranslation } from 'react-i18next';
+import Layout from '../modules/shared/components/Layout';
+import bgImg from '../assets/static/line_bg.webp';
 
 const NotFoundScreen = () => {
     const { t } = useTranslation();
 
     return (
         <Layout>
-            <Container>
-                <Typography variant="h1">
-                    {t("not_found_screen.title")}
+            <Container
+                sx={{
+                    display: { xs: 'flex', sm: 'flex', md: 'flex' },
+                    justifyContent: { xs: 'center', sm: 'center' },
+                    alignItems: { xs: 'center', sm: 'center' },
+                    textAlign: { xs: 'center' },
+                    backgroundImage: `url(${bgImg})`,
+                    backgroundSize: 'cover',
+                    backgroundPosition: 'center',
+                }}
+            >
+                <Typography
+                    variant="h4"
+                    sx={{
+                        display: { xs: 'block', md: 'none' }, // Wyświetlaj h4 tylko na xs
+                        textShadow: '2px 2px 4px rgba(0, 0, 0, 0.7)',
+                    }}
+                >
+                    {t('not_found_screen.title')}
                 </Typography>
-                <Typography variant="body1">
-                    {t("not_found_screen.subtitle")}
+                <Typography
+                    variant="h2"
+                    sx={{
+                        display: { xs: 'none', md: 'block' }, // Wyświetlaj h2 tylko na md i większych
+                        textShadow: '2px 2px 4px rgba(0, 0, 0, 0.7)',
+                    }}
+                >
+                    {t('not_found_screen.title')}
+                </Typography>
+                <Typography
+                    variant="body1"
+                    sx={{ textShadow: '2px 2px 4px rgba(0, 0, 0, 0.7)', fontSize: { xs: '1rem', sm: '1rem' } }}
+                >
+                    {t('not_found_screen.subtitle')}
                 </Typography>
             </Container>
         </Layout>
