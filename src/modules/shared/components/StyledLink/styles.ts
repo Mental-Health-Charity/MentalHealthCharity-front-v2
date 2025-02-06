ba6397de181dd49c6bc7d5dd1790b0403ec5e920
Jakub Łobos <jakubloboskontakt@gmail.com>
2025-02-06 21:16:32 +0100

    fix update articles

---
+++ src/modules/shared/components/StyledLink/styles.ts
@@ -0,0 +1,12 @@
+import { Link as MuiLink, styled } from '@mui/material';
+import { Link } from 'react-router-dom';
+
+export const StyledLink = styled(Link)<{ color: string }>`
+    font-family: 'Ubuntu', sans-serif;
+    color: ${({ theme, color }) => color || theme.palette.colors.accent};
+`;
+
+export const StyledExternalLink = styled(MuiLink)<{ color: 'primary' | 'secondary' }>`
+    font-family: 'Ubuntu', sans-serif;
+    color: ${({ theme, color }) => color || theme.palette.colors.accent};
+`;
