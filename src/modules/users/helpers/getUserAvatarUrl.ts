3d22fc2593a8f05066e63521b84c123805e07b59
Adam Styrkosz <adam963110@gmail.com>
2026-04-16 18:28:48 +0200

    Fix avatar formatting lint

--- src/modules/users/helpers/getUserAvatarUrl.ts
+++ src/modules/users/helpers/getUserAvatarUrl.ts
@@ -4,10 +4,7 @@ import { User } from "../../auth/types";
 const getUserAvatarUrl = (user?: User) => {
     if (!user) return undefined;
 
-    return (
-        resolveAssetUrl(user.user_public_profile?.avatar_url) ||
-        resolveAssetUrl(user.chat_avatar_url)
-    );
+    return resolveAssetUrl(user.user_public_profile?.avatar_url) || resolveAssetUrl(user.chat_avatar_url);
 };
 
 export default getUserAvatarUrl;
