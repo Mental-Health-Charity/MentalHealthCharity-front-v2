01e1e7dbe9fca03eb814825a8af524a0afe741be
JakubLobos <saltver2@onet.eu>
2026-08-07 02:46:34 +0200

    fix: expand participant roles for managed and special chats

--- src/modules/chat/helpers/getAllowedParticipantRoles.ts
+++ src/modules/chat/helpers/getAllowedParticipantRoles.ts
@@ -1,8 +1,20 @@
 import { Roles } from "../../users/constants";
 import { Chat } from "../types";
 
-const managedChatParticipantRoles: Roles[] = [Roles.ADMIN, Roles.VOLUNTEERSUPERVISOR, Roles.REDACTOR];
-const specialChatParticipantRoles: Roles[] = [Roles.ADMIN, Roles.VOLUNTEER, Roles.VOLUNTEERSUPERVISOR, Roles.REDACTOR];
+const managedChatParticipantRoles: Roles[] = [
+    Roles.ADMIN,
+    Roles.VOLUNTEERSUPERVISOR,
+    Roles.REDACTOR,
+    Roles.USER,
+    Roles.VOLUNTEER,
+];
+const specialChatParticipantRoles: Roles[] = [
+    Roles.ADMIN,
+    Roles.VOLUNTEER,
+    Roles.VOLUNTEERSUPERVISOR,
+    Roles.REDACTOR,
+    Roles.USER,
+];
 
 const getAllowedParticipantRoles = (chat: Pick<Chat, "is_special_chat">): Roles[] =>
     chat.is_special_chat ? specialChatParticipantRoles : managedChatParticipantRoles;
